dc0d663416d18de05c1340ee5e18e869807c44a8
Andrei Stupkin <stupkin80@gmail.com>
2021-05-03 16:30:29 +0300

    v.1.1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,14 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld helloWorldClass1 = applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld helloWorldClass2 = applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.println(helloWorldClass1.getMessage());
+        System.out.println("Ссылки helloWorldClass1 и helloWorldClass2 указывают на один объект? "
+                + (helloWorldClass1 == helloWorldClass2));
+
+        Cat cat1 = applicationContext.getBean("catBean", Cat.class);
+        Cat cat2 = applicationContext.getBean("catBean", Cat.class);
+        System.out.println("Ссылки cat1 и cat2 указывают на один объект? " + (cat1 == cat2));
     }
 }
